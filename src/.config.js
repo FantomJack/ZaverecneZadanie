64e16a2ca1cdc47b171d904fa67dce0b84ee0544
Karas Patrik <xkaras@node57.webte.fei.stuba.sk>
2024-05-05 23:00:00 +0200

    nginx conf. for code parsing from URL

--- src/.config.js
+++
@@ -1,4 +0,0 @@
-let register_api_url = 'https://node18.webte.fei.stuba.sk/ZaverecneZadanie/src/BE/auth/register.php';
-let login_api_url = 'https://node18.webte.fei.stuba.sk/ZaverecneZadanie/src/BE/auth/login.php';
-let islogged_url = 'https://node18.webte.fei.stuba.sk/ZaverecneZadanie/src/BE/auth/isloggedin.php';
-let main_page_url = 'https://node18.webte.fei.stuba.sk/ZaverecneZadanie/src/index.php'
---
+++ src/vote.php
@@ -0,0 +1,30 @@
+<?php
+// Get the full request URI
+$request_uri = $_SERVER['REQUEST_URI'];
+
+// Split the URI into parts
+$uri_parts = explode('/', $request_uri);
+
+// Get the last part of the URI, which should be your code
+$code = end($uri_parts);
+
+// Do something with the code, like display it or process it
+echo "The code is: " . htmlspecialchars($code);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+    <script src=".config.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<div id="qrcode"></div>
+
+</body>
+</html>
